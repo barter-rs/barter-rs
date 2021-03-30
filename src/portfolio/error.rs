d79ebf647932809178c5ba14624e46c6bb1fd8bd
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2021-03-30 20:25:49 +0100

    feat: add additional errors to `PortfolioError` enum *
    * `CalcProfitLossError`
    * `ParseEntryDirectionError`
    * `CannotEnterPositionWithExitFill`
    * `CannotExitPositionWithEntryFill`

--- src/portfolio/error.rs
+++ src/portfolio/error.rs
@@ -4,4 +4,16 @@ use thiserror::Error;
 pub enum PortfolioError {
     #[error("Failed to build struct due to incomplete attributes provided")]
     BuilderIncomplete(),
+
+    #[error("Failed to calculate PnL due to no Fee::TotalFee in HashMap<Fee, FeeAmount>")]
+    CalcProfitLossError(),
+
+    #[error("Failed to parse Position entry direction due to ambiguous fill quantity & Decision.")]
+    ParseEntryDirectionError(),
+
+    #[error("Cannot exit Position with an entry decision FillEvent.")]
+    CannotEnterPositionWithExitFill(),
+
+    #[error("Cannot exit Position with an entry decision FillEvent.")]
+    CannotExitPositionWithEntryFill(),
 }
