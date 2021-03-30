826078de4100793113b591f849b0b4aacb38fb48
keirbailey <keirinvestment@gmail.com>
2021-03-30 20:37:07 +0100

    feat: added repository traits required to interact with persisted portfolio

---
+++ src/portfolio/repository/error.rs
@@ -0,0 +1,20 @@
+use thiserror::Error;
+
+/// All errors generated in the barter::portfolio::repository module.
+#[derive(Error, Debug)]
+pub enum RepositoryError {
+    #[error("Failed to serialise struct to JSON")]
+    JsonSerialisationError(),
+
+    #[error("Failed to deserialise JSON to struct")]
+    JsonDeserialisationError(),
+
+    #[error("Failed to write data to the repository")]
+    WriteError(),
+
+    #[error("Failed to read data from the repository")]
+    ReadError(),
+
+    #[error("Failed to delete data from the repository")]
+    DeleteError(),
+}
