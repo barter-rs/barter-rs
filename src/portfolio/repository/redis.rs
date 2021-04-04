1da1b88fe24de821d5b07aed00b5462a0170ae84
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2021-04-04 16:02:57 +0100

    docs: add rust docs to `redis.rs`

--- src/portfolio/repository/redis.rs
+++ src/portfolio/repository/redis.rs
@@ -8,27 +8,41 @@ use crate::portfolio::error::PortfolioError;
 use crate::portfolio::error::PortfolioError::BuilderIncomplete;
 use redis::{RedisResult, ErrorKind, Connection, Commands};
 
+/// Handles the reading & writing of a [Position] to/from the persistence layer.
 pub trait PositionHandler {
+    /// Upsert the [Position] at it's position_id.
     fn set_position(&mut self, portfolio_id: &Uuid, position: &Position) -> Result<(), RepositoryError>;
+    /// Get the [Position] using it's position_id.
     fn get_position(&mut self, position_id: &String) -> Result<Option<Position>, RepositoryError>;
+    /// Remove the [Position] found at it's position_id.
     fn remove_position(&mut self, position_id: &String) -> Result<Option<Position>, RepositoryError>;
 }
 
+/// Handles the reading & writing of a Portfolio's current value to/from the persistence layer.
 pub trait ValueHandler {
+    /// Upsert the Portfolio current value as it's portfolio_id.
     fn set_current_value(&mut self, portfolio_id: &Uuid, value: f64)  -> Result<(), RepositoryError>;
+    /// Get the Portfolio current value using it's portfolio_id.
     fn get_current_value(&mut self, portfolio_id: &Uuid) -> Result<f64, RepositoryError>;
 }
 
+/// Handles the reading & writing of a Portfolio's current cash to/from the persistence layer.
 pub trait CashHandler {
+    /// Upsert the Portfolio current cash as it's portfolio_id.
     fn set_current_cash(&mut self, portfolio_id: &Uuid, cash: f64)  -> Result<(), RepositoryError>;
+    /// Get the Portfolio current cash using it's portfolio_id.
     fn get_current_cash(&mut self, portfolio_id: &Uuid) -> Result<f64, RepositoryError>;
 }
 
+/// Configuration for constructing a [RedisRepository] via the new() constructor method.
 #[derive(Debug, Deserialize)]
 pub struct Config {
     pub uri: String
 }
 
+/// Redis persisted repository that implements [PositionHandler], [ValueHandler] & [CashHandler].
+/// Used by a Portfolio implementation to persist the Portfolio state, including current value,
+/// cash & Positions.
 pub struct RedisRepository {
     conn: Connection,
 }
@@ -116,6 +130,7 @@ impl CashHandler for RedisRepository {
 }
 
 impl RedisRepository {
+    /// Constructs a new [RedisRepository] component using the provided configuration struct.
     pub fn new(connection: Connection) -> RedisRepository {
         RedisRepository::builder()
             .conn(connection)
@@ -123,10 +138,12 @@ impl RedisRepository {
             .expect("Failed to build RedisRepository")
     }
 
+    /// Returns a [RedisRepositoryBuilder] instance.
     pub fn builder() -> RedisRepositoryBuilder {
         RedisRepositoryBuilder::new()
     }
 
+    /// Establish & return a Redis connection.
     pub fn setup_redis_connection(cfg: &Config) -> Connection {
         redis::Client::open(&*cfg.uri)
             .expect("Failed to create Redis client")
@@ -134,6 +151,7 @@ impl RedisRepository {
             .expect("Failed to connect to Redis")
     }
 
+    /// Parses a f64 [RedisResult] and returns a regular Result<f64>.
     pub fn parse_read_f64_result(result: RedisResult<f64>) -> Result<f64, RepositoryError> {
         match result {
             Ok(f64_value) => Ok(f64_value),
@@ -142,6 +160,7 @@ impl RedisRepository {
     }
 }
 
+/// Builder to construct [RedisRepository] instances.
 pub struct RedisRepositoryBuilder {
     conn: Option<Connection>,
 }
@@ -169,14 +188,17 @@ impl RedisRepositoryBuilder {
     }
 }
 
+/// Returns a [Position] unique identifier given a portfolio_id, exchange & symbol.
 pub fn determine_position_id(portfolio_id: &Uuid, exchange: &String, symbol: &String) -> String {
     format!("{}_{}_{}", portfolio_id.to_string(), exchange, symbol)
 }
 
+/// Returns a unique identifier for a Portfolio's current value, given a portfolio_id.
 pub fn determine_value_id(portfolio_id: &Uuid) -> String {
     format!("{}_value", portfolio_id.to_string())
 }
 
+/// Returns a unique identifier for a Portfolio's current cash, given a portfolio_id.
 pub fn determine_cash_id(portfolio_id: &Uuid) -> String {
     format!("{}_cash", portfolio_id.to_string())
 }
