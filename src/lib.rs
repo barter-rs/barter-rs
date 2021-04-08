1507df2172422f040ffc86fb4d62d3ef405be980
keirbailey <keirinvestment@gmail.com>
2021-04-08 20:09:53 +0100

    feat: removed imports and removed links from module rustdocs

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,28 @@
-use data::{market::MarketEvent, handler::{Continuer, MarketGenerator, HistoricDataHandler}};
-use strategy::{signal::SignalEvent, strategy::{SignalGenerator, RSIStrategy}};
-use portfolio::{
-    order::OrderEvent, position::Position,
-    portfolio::{MarketUpdater, OrderGenerator, FillUpdater, PersistedMetaPortfolio},
-    repository::redis::RedisRepository, allocator::OrderAllocator, risk::OrderEvaluator
-};
-use execution::{fill::FillEvent, handler::{FillGenerator, SimulatedExecution}};
-
-/// Defines a [MarketEvent], and provides the useful traits of [Continuer] and [MarketGenerator] for
-/// handling the generation of them. Contains implementations such as the [HistoricDataHandler] that
+/// Defines a MarketEvent, and provides the useful traits of Continuer and MarketGenerator for
+/// handling the generation of them. Contains implementations such as the HistoricDataHandler that
 /// simulates a live market feed and acts as the systems heartbeat.
 pub mod data;
 
-/// Defines a [SignalEvent], and provides the [SignalGenerator] trait for handling the generation of
-/// them. Contains an example [RSIStrategy] implementation that analyses a [MarketEvent] and may
-/// generate a new [SignalEvent], an advisory signal for a Portfolio [OrderGenerator] to analyse.
+/// Defines a SignalEvent, and provides the SignalGenerator trait for handling the generation of
+/// them. Contains an example RSIStrategy implementation that analyses a MarketEvent and may
+/// generate a new SignalEvent, an advisory signal for a Portfolio OrderGenerator to analyse.
 pub mod strategy;
 
-/// Defines useful data structures such as an [OrderEvent] and [Position]. The Portfolio must
-/// interact with [MarketEvent]s, [SignalEvent]s, [OrderEvent]s, and [FillEvent]s. The useful traits
-/// [MarketUpdater], [OrderGenerator], & [FillUpdater] are provided that define the interactions
-/// with these events. Contains a [PersistedMetaPortfolio] implementation that persists state in a
-/// [RedisRepository]. This also contains example implementations of a [OrderAllocator] &
-/// [OrderEvaluator], and help the Portfolio make decisions on whether to generate [OrderEvent]s and
+/// Defines useful data structures such as an OrderEvent and Position. The Portfolio must
+/// interact with MarketEvents, SignalEvents, OrderEvents, and FillEvents. The useful traits
+/// MarketUpdater, OrderGenerator, & FillUpdater are provided that define the interactions
+/// with these events. Contains a PersistedMetaPortfolio implementation that persists state in a
+/// RedisRepository. This also contains example implementations of a OrderAllocator &
+/// OrderEvaluator, and help the Portfolio make decisions on whether to generate OrderEvents and
 /// of what size.
 pub mod portfolio;
 
-/// Defines a [FillEvent], and provides a useful trait [FillGenerator] for handling the generation
-/// of them. Contains a [SimulatedExecution] implementation that simulates a live broker execution
+/// Defines a FillEvent, and provides a useful trait FillGenerator for handling the generation
+/// of them. Contains a SimulatedExecution implementation that simulates a live broker execution
 /// for the system.
 pub mod execution;
 
-/// Defines an [Event] enum that could be a [MarketEvent], [SignalEvent], [OrderEvent] or
-/// [FillEvent].
+/// Defines an Event enum that could be a MarketEvent, SignalEvent, OrderEvent or
+/// FillEvent.
 pub mod event;
 
