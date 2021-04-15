1ab858aaa99eeeecaee1c1fc4bdab5b15cb73548
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2021-04-15 22:13:21 +0100

    build: increment package version to 0.2.9; improve overview & getting started in `lib.rs` rust docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,114 @@
+//! [Barter] is an open-source Rust library containing **high-performance**, **modular** trading
+//! engine & backtesting **components**.
+//!
+//!
+//! # Overview
+//! The **main components** are **Data**, **Strategy**, **Portfolio** & **Execution**.
+//!
+//! Each components is stand-alone & de-coupled. Their behaviour is captured
+//! in a set of useful traits that define how each component responds to external events.
+//!
+//! The **Data**, **Strategy** & **Execution components** are designed to be used by **one trading
+//! pair only** (eg/ ETH-USD on Binance). In order to **trade multiple pairs** on multiple exchanges,
+//! **many combinations** of these three components should be constructed and **run concurrently**.
+//!
+//! The **Portfolio** component is designed in order to **manage infinite trading pairs simultaneously**,
+//! creating a **centralised state machine** for managing trades across different asset classes,
+//! exchanges, and trading pairs. It's likely you might want to build an API on top of a Portfolio
+//! and run it as a micro-service. On the other hand, it can also be run in conjunction with the other
+//! components to provide a Portfolio for an individual trading pair.
+//!
+//! **Example high-level data architecture using these components in this** [README].
+//!
+//! # Getting Started
+//! ## Data Handler
+//! ```
+//! use barter::data::handler::{Config as DataConfig, HistoricDataHandler, Continuer, MarketGenerator};
+//!
+//! let config = DataConfig {
+//!     data_directory: "resources/data/".to_string(),
+//!     file_type: "csv".to_string(),
+//!     exchange: "BINANCE".to_string(),
+//!     symbol: "ETH-USD".to_string(),
+//!     timeframe: "1D".to_string(),
+//! };
+//!
+//! let mut data = HistoricDataHandler::new(&config);
+//!
+//! loop {
+//!     if data.should_continue() {
+//!         let market_event = data.generate_market().unwrap();
+//!     }
+//! }
+//! ```
+//!
+//! ## Strategy
+//! ```
+//! use barter::strategy::strategy::{Config as StrategyConfig, RSIStrategy, SignalGenerator};
+//!
+//! let config = StrategyConfig {
+//!     rsi_period: 14,
+//! };
+//!
+//! let mut strategy = RSIStrategy::new(&config);
+//! let signal_event = strategy.generate_signal(&market_event);
+//! ```
+//!
+//! ## Portfolio
+//! ```
+//! use barter::portfolio::portfolio::{Components, PersistedMetaPortfolio, MarketUpdater, OrderGenerator, FillUpdater};
+//! use barter::portfolio::allocator::DefaultAllocator;
+//! use barter::portfolio::risk::DefaultRisk;
+//! use barter::portfolio::repository::redis::RedisRepository;
+//! use barter::event::Event;
+//!
+//! let components = Components {
+//!     allocator: DefaultAllocator{ default_order_value: 100.0 },
+//!     risk: DefaultRisk{},
+//!     starting_cash: 10000.0,
+//! };
+//!
+//! let repository_config = RepositoryConfig {
+//!     uri: "redis://127.0.0.1:6969".to_string(),
+//! };
+//!
+//! let repository = RedisRepository::builder()
+//!     .conn(RedisRepository::setup_redis_connection(&repository_config))
+//!     .build().unwrap();
+//!
+//!
+//! let mut portfolio = PersistedMetaPortfolio::new(components, repository);
+//!
+//! match event {
+//!     Event::Market(market) => {
+//!         portfolio.update_from_market(&market);
+//!     }
+//!     Event::Signal(signal) => {
+//!         portfolio.generate_order(&signal);
+//!     }
+//!     Event::Order(order) => {
+//!         // Not relevant
+//!     }
+//!     Event::Fill(fill) => {
+//!         portfolio.update_from_fill(&fill);
+//!     }
+//! }
+//! ```
+//!
+//! ## Execution
+//! ```
+//! use barter::execution::handler::{SimulatedExecution, FillGenerator};
+//!
+//! let mut execution = SimulatedExecution::new();
+//!
+//! execution.generate_fill(&order_event);
+//! ```
+//!
+//! # Examples
+//!
+//! [Barter]: https://gitlab.com/open-source-keir/financial-modelling/trading/barter-rs
+//! [README]: https://crates.io/crates/barter
+
 /// Defines a MarketEvent, and provides the useful traits of Continuer and MarketGenerator for
 /// handling the generation of them. Contains implementations such as the HistoricDataHandler that
 /// simulates a live market feed and acts as the systems heartbeat.
