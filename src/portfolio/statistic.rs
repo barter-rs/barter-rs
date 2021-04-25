9af26bb31cff0a74ff65a3d1ab9c8c038cf2ae03
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2021-04-25 15:11:10 +0100

    feat: add `RollingStatistic` trait impl for `ProfitLoss` struct in meta portfolio

--- src/portfolio/statistic.rs
+++ src/portfolio/statistic.rs
@@ -3,125 +3,98 @@ use crate::execution::fill::FillEvent;
 use crate::portfolio::position::{Position, Direction};
 use crate::portfolio::error::PortfolioError;
 
-pub struct RollingStatistic {
+pub trait RollingStatistic {
+    fn init(position: &Position) -> Self;
+    fn update_from_position_exit(&mut self, position: &Position);
+}
+
+// Todo: Do I want to add 'strategy name' to SignalEvent, and use <strategy>_<symbol> as map key?
+pub struct MetaStatistics {
     profit_loss: HashMap<String, ProfitLoss>,
     // sharpe_ratio: HashMap<String, f64>,
 }
 
-impl RollingStatistic {
+impl MetaStatistics {
     pub fn new() -> Self {
         Self {
             profit_loss: HashMap::new(),
         }
     }
 
-    pub fn update_from_fill_exit(&mut self, position: &Position) {
-
-        if self.profit_loss.is_empty() {
-
-            self.profit_loss.insert(fill.symbol.clone(), ProfitLoss::init())
-
+    pub fn update_from_position_exit(&mut self, position: &Position) {
+        match self.profit_loss.get_mut(&position.symbol) {
+            None => {
+                self.profit_loss.insert(position.symbol.clone(), ProfitLoss::init(position))
+            },
+            Some(profit_loss) => {
+                profit_loss.update_from_position_exit(position)
+            }
         }
+
+        return
     }
 }
 
+/// Profit & Loss in the base currency denomination (eg/ USD).
 struct ProfitLoss {
-    long_count: f64,
-    short_count: f64,
-    avg_long_pnl_pct: f64,
-    avg_short_pnl_pct: f64,
-    avg_pnl_pct: f64,
+    long_contracts: f64,
+    long_pnl: f64,
+    long_pnl_per_contract: f64,
+    short_contracts: f64,
+    short_pnl: f64,
+    short_pnl_per_contract: f64,
+    total_contracts: f64,
+    total_pnl: f64,
+    total_pnl_per_contract: f64,
 }
 
-impl ProfitLoss {
-    pub fn init(position: &Position) -> Self {
-        let (long_count, long_pnl, short_count, short_pnl) = match position.direction {
-            Direction::Long => (1.0, position.result_profit_loss, 0.0, 0.0),
-            Direction::Short => (0.0, 0.0, 1.0, position.result_profit_loss)
+impl RollingStatistic for ProfitLoss {
+    fn init(position: &Position) -> Self {
+        let total_contracts = position.quantity.abs();
+        let total_pnl = position.result_profit_loss;
+        let total_pnl_per_contract = total_pnl / total_contracts;
+
+        let (
+            long_contracts, long_pnl, long_pnl_per_contract,
+            short_contracts, short_pnl, short_pnl_per_contract
+        ) = match position.direction {
+            Direction::Long => {
+                (total_contracts, total_pnl, total_pnl_per_contract, 0.0, 0.0, 0.0)
+            }
+            Direction::Short => {
+                (0.0, 0.0, 0.0, total_contracts, total_pnl, total_pnl_per_contract)
+            }
         };
 
-        ProfitLoss::builder()
-            .long_count(long_count)
-            .short_count(short_count)
-            .avg_long_pnl_pct(long_pnl)
-            .avg_short_pnl_pct(short_pnl)
-            .avg_pnl_pct(position.result_profit_loss)
-            .build()
-            .unwrap()
-    }
-
-    pub fn builder() -> ProfitLossBuilder {
-        ProfitLossBuilder::new()
-    }
-}
-
-pub struct ProfitLossBuilder {
-    long_count: Option<f64>,
-    short_count: Option<f64>,
-    avg_long_pnl_pct: Option<f64>,
-    avg_short_pnl_pct: Option<f64>,
-    avg_pnl_pct: Option<f64>,
-}
-
-impl ProfitLossBuilder {
-    pub fn new() -> Self {
         Self {
-            long_count: None,
-            short_count: None,
-            avg_long_pnl_pct: None,
-            avg_short_pnl_pct: None,
-            avg_pnl_pct: None,
+            long_contracts,
+            long_pnl,
+            long_pnl_per_contract,
+            short_contracts,
+            short_pnl,
+            short_pnl_per_contract,
+            total_contracts,
+            total_pnl,
+            total_pnl_per_contract
         }
     }
 
-    pub fn long_count(mut self, value: f64) -> Self {
-        self.long_count = Some(value);
-        self
-    }
-
-    pub fn short_count(mut self, value: f64) -> Self {
-        self.short_count = Some(value);
-        self
-    }
-
-    pub fn avg_long_pnl_pct(mut self, value: f64) -> Self {
-        self.avg_long_pnl_pct = Some(value);
-        self
-    }
-
-    pub fn avg_short_pnl_pct(mut self, value: f64) -> Self {
-        self.avg_short_pnl_pct = Some(value);
-        self
-    }
-
-    pub fn avg_pnl_pct(mut self, value: f64) -> Self {
-        self.avg_pnl_pct = Some(value);
-        self
-    }
-
-    pub fn build(self) -> Result<ProfitLoss, PortfolioError> {
-        if let (
-            Some(long_count),
-            Some(short_count),
-            Some(avg_long_pnl_pct),
-            Some(avg_short_pnl_pct),
-            Some(avg_pnl_pct),
-        ) = (
-            self.long_count,
-            self.short_count,
-            self.avg_long_pnl_pct,
-            self.avg_short_pnl_pct,
-            self.avg_pnl_pct,
-        ) {
-            Ok(ProfitLoss {
-                long_count,
-                short_count,
-                avg_long_pnl_pct,
-                avg_short_pnl_pct,
-                avg_pnl_pct,
-            })
-        } else {
-            Err(PortfolioError::BuilderIncomplete)
+    fn update_from_position_exit(&mut self, position: &Position) {
+        match position.direction {
+            Direction::Long => {
+                self.long_contracts += position.quantity.abs();
+                self.long_pnl += position.result_profit_loss;
+                self.long_pnl_per_contract = self.long_pnl / self.long_contracts;
+            }
+            Direction::Short => {
+                self.short_contracts += position.quantity.abs();
+                self.short_pnl += position.result_profit_loss;
+                self.short_pnl_per_contract = self.short_pnl / self.short_contracts;
+            }
         }
+
+        self.total_contracts += position.quantity.abs();
+        self.total_pnl += position.result_profit_loss;
+        self.total_pnl_per_contract = self.total_pnl / self.total_contracts;
     }
 }
