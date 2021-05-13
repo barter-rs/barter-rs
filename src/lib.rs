4aefbd2aeb85eb249b22879fb30cf671f3b91d0d
keirbailey <keirinvestment@gmail.com>
2021-05-13 19:19:45 +0100

    feat: Improved statistic & dispersion documentation comments

--- src/lib.rs
+++ src/lib.rs
@@ -143,7 +143,8 @@ pub mod execution;
 /// FillEvent.
 pub mod event;
 
-/// Defines various performance metrics that can be used to evaluate trading.
+/// Defines various iterative statistical methods that can be used to calculate trading performance
+/// metrics in one-pass .
 pub mod statistic;
 
 #[macro_use] extern crate prettytable;
