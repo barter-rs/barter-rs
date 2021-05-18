74d41f7208f73dc98bf3f5c3c77469460372689e
keirbailey <keirinvestment@gmail.com>
2021-05-18 16:56:08 +0100

    feat: draft drawdown metric

--- src/statistic/metric/ratio.rs
+++ src/statistic/metric/ratio.rs
@@ -98,43 +98,43 @@ impl SortinoRatio {
     }
 }
 
-#[derive(Debug, Clone, PartialOrd, PartialEq)]
-pub struct CalmarRatio {
-    pub risk_free_return: f64,
-    pub trades_per_day: f64,
-    pub calmar_ratio_per_trade: f64,
-}
-
-impl Ratio for CalmarRatio {
-    fn init(risk_free_return: f64) -> Self {
-        Self {
-            risk_free_return,
-            trades_per_day: 0.0,
-            calmar_ratio_per_trade: 0.0
-        }
-    }
-
-    fn ratio(&self) -> f64 {
-        self.calmar_ratio_per_trade
-    }
-
-    fn trades_per_day(&self) -> f64 {
-        self.trades_per_day
-    }
-}
-
-impl CalmarRatio {
-    pub fn update(&mut self, pnl_returns: &PnLReturnSummary, max_drawdown: f64) {
-        // Update Trades Per Day
-        self.trades_per_day = pnl_returns.trades_per_day;
-
-        // Calculate Calmar Ratio Per Trade // Todo: Ensure max_drawdown isn't periodised...
-        self.calmar_ratio_per_trade = match max_drawdown == 0.0 {
-            true => 0.0,
-            false => (pnl_returns.total.mean - self.risk_free_return) / max_drawdown,
-        };
-    }
-}
+// #[derive(Debug, Clone, PartialOrd, PartialEq)]
+// pub struct CalmarRatio {
+//     pub risk_free_return: f64,
+//     pub trades_per_day: f64,
+//     pub calmar_ratio_per_trade: f64,
+// }
+//
+// impl Ratio for CalmarRatio {
+//     fn init(risk_free_return: f64) -> Self {
+//         Self {
+//             risk_free_return,
+//             trades_per_day: 0.0,
+//             calmar_ratio_per_trade: 0.0
+//         }
+//     }
+//
+//     fn ratio(&self) -> f64 {
+//         self.calmar_ratio_per_trade
+//     }
+//
+//     fn trades_per_day(&self) -> f64 {
+//         self.trades_per_day
+//     }
+// }
+//
+// impl CalmarRatio {
+//     pub fn update(&mut self, pnl_returns: &PnLReturnSummary, max_drawdown: f64) {
+//         // Update Trades Per Day
+//         self.trades_per_day = pnl_returns.trades_per_day;
+//
+//         // Calculate Calmar Ratio Per Trade // Todo: Ensure max_drawdown isn't periodised...
+//         self.calmar_ratio_per_trade = match max_drawdown == 0.0 {
+//             true => 0.0,
+//             false => (pnl_returns.total.mean - self.risk_free_return) / max_drawdown,
+//         };
+//     }
+// }
 
 pub fn calculate_daily(ratio_per_trade: f64, trades_per_day: f64) -> f64 {
     ratio_per_trade * trades_per_day.sqrt()
