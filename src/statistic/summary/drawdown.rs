ae9b07b9d2da3fb98134fcdbc6efe1a66e5beb5b
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2021-05-20 09:10:51 +0100

    feat: add `pnl_returns` to `TradingSummary.print()`; add `DrawdownSummary.print()`

--- src/statistic/summary/drawdown.rs
+++ src/statistic/summary/drawdown.rs
@@ -1,6 +1,7 @@
 use crate::statistic::metric::drawdown::{Drawdown, AvgDrawdown, MaxDrawdown};
-use crate::statistic::summary::trading::PositionSummariser;
+use crate::statistic::summary::trading::{PositionSummariser, TablePrinter};
 use crate::portfolio::position::Position;
+use prettytable::{Row, Table};
 
 #[derive(Debug, Clone, PartialOrd, PartialEq)]
 pub struct DrawdownSummary {
@@ -11,7 +12,35 @@ pub struct DrawdownSummary {
 
 impl PositionSummariser for DrawdownSummary {
     fn update(&mut self, position: &Position) {
+        // Only update DrawdownSummary with closed Positions
+        let equity_point = match &position.meta.exit_equity_point {
+            None => return,
+            Some(equity_point) => equity_point,
+        };
 
+        // Updates
+        if let Some(ended_drawdown) = self.current_drawdown.update(equity_point) {
+            self.avg_drawdown.update(&ended_drawdown);
+            self.max_drawdown.update(&ended_drawdown);
+        }
+    }
+}
+
+impl TablePrinter for DrawdownSummary {
+    fn print(&self) {
+        let mut drawdown_summary = Table::new();
+
+        let titles = vec!["",
+            "Max Drawdown", "Max Drawdown Duration", "Avg. Drawdown", "Avg. Drawdown Duration",
+        ];
+
+        drawdown_summary.add_row(row!["Total",
+            self.max_drawdown.drawdown.drawdown, self.max_drawdown.drawdown.duration.to_string(),
+            self.avg_drawdown.mean_drawdown, self.avg_drawdown.mean_duration.to_string()
+        ]);
+
+        drawdown_summary.set_titles(Row::from(titles));
+        drawdown_summary.printstd();
     }
 }
 
