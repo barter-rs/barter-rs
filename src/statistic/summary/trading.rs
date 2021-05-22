8a1794d899862c6c04bf9fc0ac0e6c2b4230781c
keirbailey <keirinvestment@gmail.com>
2021-05-22 12:07:59 +0100

    feat: added calmar ratio to tear sheet

--- src/statistic/summary/trading.rs
+++ src/statistic/summary/trading.rs
@@ -1,7 +1,7 @@
 use crate::portfolio::position::Position;
 use serde::Deserialize;
 use crate::statistic::summary::pnl::PnLReturnSummary;
-use crate::statistic::metric::ratio::{SharpeRatio, SortinoRatio, Ratio};
+use crate::statistic::metric::ratio::{SharpeRatio, SortinoRatio, Ratio, CalmarRatio};
 use prettytable::{Row, Table};
 use chrono::{DateTime, Utc, Duration};
 use crate::statistic::summary::drawdown::DrawdownSummary;
@@ -68,7 +68,7 @@ impl TradingSummary {
 pub struct TearSheet {
     sharpe_ratio: SharpeRatio,
     sortino_ratio: SortinoRatio,
-    // calmar_ratio: CalmarRatio,
+    calmar_ratio: CalmarRatio,
 }
 
 impl TearSheet {
@@ -76,14 +76,14 @@ impl TearSheet {
         Self {
             sharpe_ratio: SharpeRatio::init(risk_free_return),
             sortino_ratio: SortinoRatio::init(risk_free_return),
-            // calmar_ratio: CalmarRatio::init(risk_free_return),
+            calmar_ratio: CalmarRatio::init(risk_free_return),
         }
     }
 
-    pub fn update(&mut self, pnl_returns: &PnLReturnSummary, _: &DrawdownSummary) {
+    pub fn update(&mut self, pnl_returns: &PnLReturnSummary, drawdown: &DrawdownSummary) {
         self.sharpe_ratio.update(pnl_returns);
         self.sortino_ratio.update(pnl_returns);
-        // self.calmar_ratio.update(pnl_return_view, self.drawdown.max_drawdown);
+        self.calmar_ratio.update(pnl_returns, drawdown.max_drawdown.drawdown.drawdown);
     }
 }
 
@@ -91,12 +91,12 @@ impl TablePrinter for TearSheet {
     fn print(&self) {
         let mut tear_sheet = Table::new();
 
-        let titles = vec!["", "Sharpe Ratio", "Sortino Ratio"];
+        let titles = vec!["", "Sharpe Ratio", "Sortino Ratio", "Calmar Ratio"];
 
         tear_sheet.add_row(row!["Total",
             format!("{:.3}", self.sharpe_ratio.daily()),
             format!("{:.3}", self.sortino_ratio.daily()),
-            // self.calmar_ratio.calculate_daily().to_string(),
+            format!("{:.3}", self.calmar_ratio.daily()),
         ]);
 
         tear_sheet.set_titles(Row::from(titles));
