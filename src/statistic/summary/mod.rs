c915b633ca9981776ed0ecf95022d707bf04edb9
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2021-11-02 13:15:22 +0000

    refactor: move traits in `summary` module into `mod.rs`

--- src/statistic/summary/mod.rs
+++ src/statistic/summary/mod.rs
@@ -2,3 +2,18 @@ pub mod data;
 pub mod drawdown;
 pub mod pnl;
 pub mod trading;
+
+use crate::portfolio::position::Position;
+
+pub trait PositionSummariser {
+    fn update(&mut self, position: &Position);
+    fn generate_summary(&mut self, positions: &Vec<Position>) {
+        for position in positions.iter() {
+            self.update(position)
+        }
+    }
+}
+
+pub trait TablePrinter {
+    fn print(&self);
+}
