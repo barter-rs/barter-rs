d5aeb03e270d07f89d1c508ae6f3aa635e70afe7
JustAStream <keirinvestment@gmail.com>
2021-11-02 13:05:43 +0000

    feat: moved traits into mod.rs

--- src/portfolio/mod.rs
+++ src/portfolio/mod.rs
@@ -1,3 +1,9 @@
+use crate::data::market::MarketEvent;
+use crate::execution::fill::FillEvent;
+use crate::portfolio::error::PortfolioError;
+use crate::portfolio::order::OrderEvent;
+use crate::strategy::signal::SignalEvent;
+
 pub mod allocator;
 pub mod error;
 pub mod order;
@@ -5,3 +11,27 @@ pub mod portfolio;
 pub mod position;
 pub mod repository;
 pub mod risk;
+
+/// Updates the Portfolio from an input [MarketEvent].
+pub trait MarketUpdater {
+    /// Determines if the Portfolio has any open [Position]s relating to the input [MarketEvent],
+    /// and if so updates it using the market data.
+    fn update_from_market(&mut self, market: &MarketEvent) -> Result<(), PortfolioError>;
+}
+
+/// May generate an [OrderEvent] from an input advisory [SignalEvent].
+pub trait OrderGenerator {
+    /// May generate an [OrderEvent] after analysing an input advisory [SignalEvent].
+    fn generate_order(
+        &mut self,
+        signal: &SignalEvent,
+    ) -> Result<Option<OrderEvent>, PortfolioError>;
+}
+
+/// Updates the Portfolio from an input [FillEvent].
+pub trait FillUpdater {
+    /// Updates the Portfolio state using the input [FillEvent]. The [FillEvent] triggers a
+    /// [Position] entry or exit, and the Portfolio updates key fields such as current_cash and
+    /// current_value accordingly.
+    fn update_from_fill(&mut self, fill: &FillEvent) -> Result<(), PortfolioError>;
+}
