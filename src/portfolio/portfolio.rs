6eb0550a6076fd910d2fb1b949c229fd724cc9b8
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2021-11-02 13:05:43 +0000

    refactor: move traits in `portfolio` module into `mod.rs`

--- src/portfolio/portfolio.rs
+++ src/portfolio/portfolio.rs
@@ -11,33 +11,10 @@ use crate::portfolio::repository::redis::{
 };
 use crate::portfolio::risk::{DefaultRisk, OrderEvaluator};
 use crate::strategy::signal::{Decision, SignalEvent, SignalStrength};
+use crate::portfolio::{FillUpdater, MarketUpdater, OrderGenerator};
 use chrono::Utc;
 use std::collections::HashMap;
 use uuid::Uuid;
-
-/// Updates the Portfolio from an input [MarketEvent].
-pub trait MarketUpdater {
-    /// Determines if the Portfolio has any open [Position]s relating to the input [MarketEvent],
-    /// and if so updates it using the market data.
-    fn update_from_market(&mut self, market: &MarketEvent) -> Result<(), PortfolioError>;
-}
-
-/// May generate an [OrderEvent] from an input advisory [SignalEvent].
-pub trait OrderGenerator {
-    /// May generate an [OrderEvent] after analysing an input advisory [SignalEvent].
-    fn generate_order(
-        &mut self,
-        signal: &SignalEvent,
-    ) -> Result<Option<OrderEvent>, PortfolioError>;
-}
-
-/// Updates the Portfolio from an input [FillEvent].
-pub trait FillUpdater {
-    /// Updates the Portfolio state using the input [FillEvent]. The [FillEvent] triggers a
-    /// [Position] entry or exit, and the Portfolio updates key fields such as current_cash and
-    /// current_value accordingly.
-    fn update_from_fill(&mut self, fill: &FillEvent) -> Result<(), PortfolioError>;
-}
 
 /// Components for construction a [MetaPortfolio] via the new() constructor method.
 #[derive(Debug)]
