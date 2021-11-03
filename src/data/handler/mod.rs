e2df507574813e5571c3690be34491fd46c5f1ac
JustAStream <keirinvestment@gmail.com>
2021-11-03 14:50:08 +0000

    feat: Continuer trait returns Continuation::stop/continue enum for readability!

--- src/data/handler/mod.rs
+++ src/data/handler/mod.rs
@@ -2,15 +2,23 @@ pub mod historic;
 pub mod live;
 
 use crate::data::market::MarketEvent;
+use serde::{Deserialize, Serialize};
 
 /// Determines if a process should continue.
 pub trait Continuer {
     /// Return true if a process should continue.
-    fn should_continue(&self) -> bool;
+    fn should_continue(&mut self) -> Continuation;
 }
 
 /// Generates the latest [MarketEvent], acting as the system heartbeat.
 pub trait MarketGenerator {
     /// Return the latest [MarketEvent].
     fn generate_market(&mut self) -> Option<MarketEvent>;
+}
+
+/// Returned by a [Continuer] to communicate if a process should continue.
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Continuation {
+    Continue,
+    Stop,
 }
