61f74f0bcceb4071c6d31bc108d90b0b41be7443
JustAStream <keirinvestment@gmail.com>
2021-11-27 11:35:59 +0000

    feat: basic eventsink impl

--- src/event.rs
+++ src/event.rs
@@ -1,7 +1,10 @@
 use crate::data::market::MarketEvent;
-use crate::execution::fill::FillEvent;
-use crate::portfolio::order::OrderEvent;
 use crate::strategy::signal::SignalEvent;
+use crate::portfolio::order::OrderEvent;
+use crate::portfolio::position::Position;
+use crate::execution::fill::FillEvent;
+use tokio::sync::mpsc;
+use tracing::warn;
 
 #[derive(Debug)]
 pub enum Event {
@@ -9,4 +12,22 @@ pub enum Event {
     Signal(SignalEvent),
     Order(OrderEvent),
     Fill(FillEvent),
+    ClosedPosition(Position)
+}
+
+#[derive(Debug)]
+pub struct EventSink {
+    event_tx: mpsc::UnboundedSender<Event>,
+}
+
+impl EventSink {
+    pub fn new(event_tx: mpsc::UnboundedSender<Event>) -> Self {
+        Self { event_tx }
+    }
+
+    pub fn send(&mut self, event: Event) {
+        if self.event_tx.send(event).is_err() {
+            warn!("EventSink receiver has been dropped & cannot send Events");
+        }
+    }
 }
