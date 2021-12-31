86c90c47db2532a15b8a9019efadf431a021ff3d
JustAStream <keirinvestment@gmail.com>
2021-12-31 16:04:21 +0000

    feat: shell of command issuer

--- src/engine/commander.rs
+++ src/engine/commander.rs
@@ -1,3 +1,28 @@
+use crate::engine::Message;
+use crate::Market;
 
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct Commander;
+
+impl Commander {
+
+    pub fn terminate_traders(&self, message: Message) {
+
+    }
+
+    pub fn exit_position(&self, market: Market) {
+        // Determine Trader to send it to using Market, since EngineId is latently known from
+        // creation of Engine - Trader channel relationships -> only this Engine has access to this
+        // these Traders so they are linked by EngineId
+
+        // Require position Id
+        // Require market
+    }
+
+    pub fn exit_all_positions(&self) {
+
+    }
+
+
+
+}
