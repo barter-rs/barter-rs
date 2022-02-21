7a464e93af471f6c8d34acfed07cbdd7574bfb97
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2022-02-21 20:50:41 +0000

    feat: add `Engine.generate_session_summary()` impl; rename & replace some traits *
    * add `Engine.generate_session_summary()` impl
    * rename `FillGenerator` trait into `ExecutionClient`
    * replace `TablePrinter` trait with `TableBuilder`

--- src/statistic/summary/mod.rs
+++ src/statistic/summary/mod.rs
@@ -4,6 +4,7 @@ pub mod pnl;
 pub mod trading;
 
 use crate::portfolio::position::Position;
+use prettytable::{Cell, Row, Table};
 
 pub trait Initialiser {
     type Config: Copy;
@@ -21,4 +22,63 @@ pub trait PositionSummariser: Copy {
 
 pub trait TablePrinter {
     fn print(&self);
+}
+
+pub trait TableBuilder {
+    fn titles(&self) -> Row;
+    fn row(&self) -> Row;
+    fn table(&self, id_cell: &str) -> Table {
+        let mut table = Table::new();
+
+        let mut titles = self.titles();
+        titles.insert_cell(0, Cell::new(""));
+        table.set_titles(titles);
+
+        let mut row = self.row();
+        row.insert_cell(0, Cell::new(id_cell));
+        table.add_row(row);
+
+        table
+    }
+    fn table_with<T: TableBuilder>(&self, id_cell: &str, another: (T, &str)) -> Table {
+        let mut table = Table::new();
+
+        let mut titles = self.titles();
+        titles.insert_cell(0, Cell::new(""));
+        table.set_titles(titles);
+
+        let mut first_row = self.row();
+        first_row.insert_cell(0, Cell::new(id_cell));
+        table.add_row(first_row);
+
+        let mut another_row = another.0.row();
+        another_row.insert_cell(0, Cell::new(another.1));
+        table.add_row(another_row);
+
+        table
+    }
+}
+
+pub fn combine<'a, Iter, T>(builders: Iter) -> Table
+where
+    Iter: Iterator<Item = (String, T)>,
+    T: TableBuilder
+{
+    builders
+        .enumerate()
+        .fold(Table::new(), |mut table, (index, (id, builder))| {
+            // Set Table titles using the first builder
+            if index == 0 {
+                let mut titles = builder.titles();
+                titles.insert_cell(0, Cell::new(""));
+                table.set_titles(titles);
+            }
+
+            // Add rows for each builder
+            let mut row = builder.row();
+            row.insert_cell(0, Cell::new(&id));
+            table.add_row(row);
+
+            table
+        })
 }
