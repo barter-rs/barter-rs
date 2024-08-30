8c6eabdfa0d3a8c98800ac458fa41f47536cb85b
sina <khalili@sfu.ca>
2024-08-30 16:24:34 -0700

    Remove bybit from l1 streams example (since the l1 orderbook is a bit borked

--- barter-data/examples/order_books_l1_streams_multi_exchange.rs
+++ barter-data/examples/order_books_l1_streams_multi_exchange.rs
@@ -1,7 +1,6 @@
 use barter_data::{
     exchange::{
         binance::{futures::BinanceFuturesUsd, spot::BinanceSpot},
-        bybit::spot::BybitSpot,
         kraken::Kraken,
     },
     streams::Streams,
@@ -34,7 +33,6 @@ async fn main() {
             (Kraken, "matic", "usd", InstrumentKind::Spot, OrderBooksL1),
             (Kraken, "dot", "usd", InstrumentKind::Spot, OrderBooksL1),
         ])
-        .subscribe([(BybitSpot::default(), "btc", "usdt", InstrumentKind::Spot, OrderBooksL1)])
         .init()
         .await
         .unwrap();
