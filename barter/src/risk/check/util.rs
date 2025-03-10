6bd0176f7688365fe2a851bf2054ec70592ba816
JustAStream <justastream.code@gmail.com>
2025-03-10 20:09:19 +0000

    feat: utility for calculating the delta associated with an instrument quantity

--- barter/src/risk/check/util.rs
+++ barter/src/risk/check/util.rs
@@ -1,3 +1,4 @@
+use barter_instrument::Side;
 use rust_decimal::Decimal;
 
 /// Calculates the notional value in quote asset given the quantity, price and contract_size.
@@ -11,7 +12,7 @@ use rust_decimal::Decimal;
 /// * `price` - The price per contract/unit
 ///   - For standard instruments, this is typically the current market price
 ///   - For option instruments, this should be the strike price
-/// * `contract_size` - The multiplier that determines the actual exposure per contract
+/// * `contract_size` - Multiplier that determines the actual exposure per contract
 pub fn calculate_quote_notional(
     quantity: Decimal,
     price: Decimal,
@@ -30,4 +31,32 @@ pub fn calculate_abs_percent_difference(current: Decimal, other: Decimal) -> Opt
 
     // Calculate percentage difference relative to other
     price_diff.checked_div(other)
+}
+
+/// Calculate the total delta for some quantity of "in kind" units.
+///
+/// Delta is a measure of how an instruments price changes relative to the underlying asset.
+///
+/// A positive return value indicates long exposure to the underlying, while negative values
+/// indicate short exposure.
+///
+/// # Arguments
+/// * `instrument_delta` - Delta of the instrument
+///   - For Spot, Perpetual, and Futures, this is 1.0.
+///   - For Options, this is between -1.0 and 1.0.
+/// * `contract_size` - Multiplier that determines the actual exposure per contract
+/// * `side` - Quantity direction, `Side::Buy` (LONG) or `Side::Sell` (SHORT)
+/// * `quantity_in_kind` - In kind quantity
+pub fn calculate_delta(
+    instrument_delta: Decimal,
+    contract_size: Decimal,
+    side: Side,
+    quantity_in_kind: Decimal,
+) -> Decimal {
+    let delta = instrument_delta * (quantity_in_kind * contract_size);
+
+    match side {
+        Side::Buy => delta,
+        Side::Sell => -delta,
+    }
 }
