97059c7e8d6ca21bfa99fb6ab5525f26d7a85ba8
jna <atherley@adobe.com>
2025-03-20 20:53:32 +0100

    Refactor to use From trait where possible

--- barter-data/src/provider/databento/transform.rs
+++ barter-data/src/provider/databento/transform.rs
@@ -2,7 +2,6 @@ use chrono::DateTime;
 use databento::dbn::{Action, MboMsg, RecordRef, UNDEF_PRICE};
 use rust_decimal::Decimal;
 use rust_decimal::prelude::FromPrimitive;
-use tracing::info;
 use barter_instrument::exchange::ExchangeId;
 use barter_instrument::instrument::InstrumentIndex;
 use barter_instrument::Side;
@@ -11,99 +10,52 @@ use crate::event::{DataKind, MarketEvent};
 use crate::provider::databento::DatabentoSide;
 use crate::subscription::book::{OrderBookAction, OrderBookEvent, OrderBookUpdate};
 
-pub fn transform_mb0(mbo: &MboMsg) -> Result<Option<MarketEvent<InstrumentIndex, DataKind>>, DataError> {
-
+fn to_market_event(mbo: MboMsg, action: OrderBookAction) -> MarketEvent<InstrumentIndex, DataKind> {
     let time_exchange = DateTime::from_timestamp_nanos(mbo.ts_recv as i64).to_utc();
+    let time_received = chrono::Utc::now();
+    let exchange = ExchangeId::Other;
+    let instrument = InstrumentIndex(0);
+    let kind = DataKind::from(
+        OrderBookEvent::IncrementalUpdate(OrderBookUpdate::from((mbo, action))));
 
-    if mbo.flags.is_snapshot() && !mbo.flags.is_last() {
-        return match mbo.action() {
-            Ok(Action::Add) => {
-                let side = mbo.side()?;
-                let price = mbo.price_f64();
+    MarketEvent {
+        time_exchange,
+        time_received,
+        exchange,
+        instrument,
+        kind,
+    }
+}
 
-                info!("Snapshot received {snapshot} {action} {price} {size} {side}",
-                    snapshot = true, action = "add", price = price, size = mbo.size, side = Side::from(DatabentoSide::from(side)));
+impl From<(MboMsg, OrderBookAction)> for OrderBookUpdate {
+    fn from(value: (MboMsg, OrderBookAction)) -> Self {
+        let (mbo, action) = value;
+        let side = mbo.side().unwrap();
+        let price = mbo.price_f64();
 
-                Ok(Some(MarketEvent {
-                    time_exchange: time_exchange.clone(),
-                    time_received: chrono::Utc::now(),
-                    exchange: ExchangeId::Other,
-                    instrument: InstrumentIndex(0),
-                    kind: DataKind::from(OrderBookEvent::IncrementalUpdate(OrderBookUpdate {
-                        order_id: Some(mbo.order_id.to_string()),
-                        price: Decimal::from_f64(price).unwrap(),
-                        amount: Decimal::from(mbo.size),
-                        side: Side::from(DatabentoSide::from(side)),
-                        sequence: mbo.sequence as u64,
-                        action: OrderBookAction::Add,
-                    })),
-                }))
-            }
-            _ => {
-                return Ok(None)
-            }
+        OrderBookUpdate {
+            order_id: Some(mbo.order_id.to_string()),
+            price: Decimal::from_f64(price).unwrap(),
+            amount: Decimal::from(mbo.size),
+            side: Side::from(DatabentoSide::from(side)),
+            sequence: mbo.sequence as u64,
+            action,
         }
     }
+}
 
+pub fn transform_mb0(mbo: &MboMsg) -> Result<Option<MarketEvent<InstrumentIndex, DataKind>>, DataError> {
     if mbo.price == UNDEF_PRICE {
         return Ok(None)
     }
 
-    let side = mbo.side()?;
-    let price = mbo.price_f64();
-
     match mbo.action() {
-        Ok(Action::Add) => {
-            Ok(Some(MarketEvent {
-                time_exchange: time_exchange.clone(),
-                time_received: chrono::Utc::now(),
-                exchange: ExchangeId::Other,
-                instrument: InstrumentIndex(0),
-                kind: DataKind::from(OrderBookEvent::IncrementalUpdate(OrderBookUpdate {
-                    order_id: Some(mbo.order_id.to_string()),
-                    price: Decimal::from_f64(price).unwrap(),
-                    amount: Decimal::from(mbo.size),
-                    side: Side::from(DatabentoSide::from(side)),
-                    sequence: mbo.sequence as u64,
-                    action: OrderBookAction::Add,
-                })),
-            }))
-        }
-        Ok(Action::Modify) => {
-            Ok(Some(MarketEvent {
-                time_exchange: time_exchange.clone(),
-                time_received: chrono::Utc::now(),
-                exchange: ExchangeId::Other,
-                instrument: InstrumentIndex(0),
-                kind: DataKind::from(OrderBookEvent::IncrementalUpdate(OrderBookUpdate {
-                    order_id: Some(mbo.order_id.to_string()),
-                    price: Decimal::from_f64(price).unwrap(),
-                    amount: Decimal::from(mbo.size),
-                    side: Side::from(DatabentoSide::from(side)),
-                    sequence: mbo.sequence as u64,
-                    action: OrderBookAction::Modify,
-                })),
-            }))
-        },
-        Ok(Action::Cancel) => {
-            Ok(Some(MarketEvent {
-                time_exchange: time_exchange.clone(),
-                time_received: chrono::Utc::now(),
-                exchange: ExchangeId::Other,
-                instrument: InstrumentIndex(0),
-                kind: DataKind::from(OrderBookEvent::IncrementalUpdate(OrderBookUpdate {
-                    order_id: Some(mbo.order_id.to_string()),
-                    price: Decimal::from_f64(price).unwrap(),
-                    amount: Decimal::from(mbo.size),
-                    side: Side::from(DatabentoSide::from(side)),
-                    sequence: mbo.sequence as u64,
-                    action: OrderBookAction::Cancel,
-                })),
-            }))
-        },
+        Ok(Action::Add) => Ok(Some(to_market_event(mbo.clone(), OrderBookAction::Add))),
+        Ok(Action::Modify) => Ok(Some(to_market_event(mbo.clone(), OrderBookAction::Modify))),
+        Ok(Action::Cancel) => Ok(Some(to_market_event(mbo.clone(), OrderBookAction::Cancel))),
         Ok(Action::Clear) => {
             Ok(Some(MarketEvent {
-                time_exchange: time_exchange.clone(),
+                time_exchange: DateTime::from_timestamp_nanos(mbo.ts_recv as i64).to_utc(),
                 time_received: chrono::Utc::now(),
                 exchange: ExchangeId::Other,
                 instrument: InstrumentIndex(0),
