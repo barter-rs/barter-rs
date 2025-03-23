ac9d78239c96ebcb1a02a140ce7f60089b58687f
JustAStream <justastream.code@gmail.com>
2025-03-23 21:45:38 +0000

    fix: Asset & Instrument names use Cow<'de, str> for impl Deserialize

--- barter-instrument/src/instrument/quote.rs
+++ barter-instrument/src/instrument/quote.rs
@@ -8,10 +8,12 @@ pub enum InstrumentQuoteAsset {
     /// "In-kind" pricing (unusual) using the underlying base asset as the quote asset.
     ///
     /// For example, if some derivative for underlying=btc_usdt was quoted in btc.
+    #[serde(alias = "underlying_base")]
     UnderlyingBase,
 
     /// Standard pricing using the underlying quote as the quote asset.
     ///
     /// For example, all spot instruments are quoted in the (underlying) quote asset.
+    #[serde(alias = "underlying_quote")]
     UnderlyingQuote,
 }
