fb57a0ffe6ab622460c5f87d2dc577f804ede7c1
JustAStream <justastream.code@gmail.com>
2025-03-30 22:09:01 +0100

    feat: backtest utilities & concurrent backtest example

---
+++ barter/examples/backtests_concurrent.rs
@@ -0,0 +1,137 @@
+use barter::{
+    backtest::{
+        BacktestArgsConstant, BacktestArgsDynamic, market_data::MarketDataInMemory, run_backtests,
+    },
+    engine::state::{EngineState, instrument::data::DefaultInstrumentMarketData},
+    risk::{DefaultRiskManager, DefaultRiskManagerState},
+    statistic::time::Daily,
+    strategy::{DefaultStrategy, DefaultStrategyState},
+    system::config::SystemConfig,
+};
+use barter_data::streams::consumer::MarketStreamEvent;
+use barter_instrument::index::IndexedInstruments;
+use rust_decimal::Decimal;
+use serde::Deserialize;
+use smol_str::{SmolStr, ToSmolStr};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    sync::Arc,
+};
+
+const CONFIG_PATH: &str = "barter/examples/config/backtest_config.json";
+const FILE_PATH_MARKET_DATA_INDEXED: &str =
+    "barter/examples/data/binance_spot_trades_l1_btcusdt_ethusdt_solusdt.json";
+const NUM_BACKTESTS: usize = 10000;
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub risk_free_return: Decimal,
+    pub system: SystemConfig,
+}
+
+#[tokio::main]
+async fn main() {
+    // Initialise Tracing
+    barter::logging::init_logging();
+
+    let Config {
+        risk_free_return,
+        system: SystemConfig {
+            instruments,
+            executions,
+        },
+    } = load_config();
+
+    // Construct IndexedInstruments
+    let instruments = IndexedInstruments::new(instruments);
+
+    // Initialise MarketData
+    let market_events = market_data_from_file(FILE_PATH_MARKET_DATA_INDEXED);
+    let market_data = MarketDataInMemory::new(Arc::new(market_events));
+
+    // Construct constant backtest arguments
+    let args_constant = Arc::new(BacktestArgsConstant {
+        instruments,
+        executions,
+        market_data,
+        summary_interval: Daily,
+    });
+
+    // Define dummy dynamic backtest arguments
+    let dynamic_arg = BacktestArgsDynamic {
+        id: SmolStr::default(),
+        risk_free_return,
+        strategy: DefaultStrategy::<
+            EngineState<DefaultInstrumentMarketData, DefaultStrategyState, DefaultRiskManagerState>,
+        >::default(),
+        risk: DefaultRiskManager::<
+            EngineState<DefaultInstrumentMarketData, DefaultStrategyState, DefaultRiskManagerState>,
+        >::default(),
+    };
+
+    // Generate dummy iterator of cloned dynamic arguments
+    // Note that concurrent backtests should be run with different BacktestArgsDynamic!
+    let args_dynamic_iter = (0..NUM_BACKTESTS).map(|index| {
+        let mut dynamic_args = dynamic_arg.clone();
+        dynamic_args.id = index.to_smolstr();
+        dynamic_args
+    });
+
+    let mut summary = run_backtests(args_constant, args_dynamic_iter)
+        .await
+        .unwrap();
+
+    // Analyse backtest summaries...
+    println!("\nNum Backtests: {}", summary.num_backtests);
+    println!("Duration: {:?}", summary.duration);
+    // For example, find the backtest with the highest cumulative PnL
+    summary.summaries.sort_by(|a, b| {
+        let backtest_a_total_pnl = a
+            .trading_summary
+            .instruments
+            .values()
+            .map(|tear| tear.pnl)
+            .sum::<Decimal>();
+        let backtest_b_total_pnl = b
+            .trading_summary
+            .instruments
+            .values()
+            .map(|tear| tear.pnl)
+            .sum::<Decimal>();
+
+        backtest_a_total_pnl.cmp(&backtest_b_total_pnl).reverse()
+    });
+    let best_cumulative_sharpe = summary.summaries.first().unwrap();
+
+    println!(
+        "\nBest Cumulative Sharpe: BacktestId = {}",
+        best_cumulative_sharpe.id
+    );
+    best_cumulative_sharpe.trading_summary.print_summary()
+}
+
+pub fn load_config() -> Config {
+    let file = File::open(CONFIG_PATH).expect("Failed to open config file");
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader).expect("Failed to parse config file")
+}
+
+pub fn market_data_from_file<InstrumentKey, Kind>(
+    file_path: &str,
+) -> Vec<MarketStreamEvent<InstrumentKey, Kind>>
+where
+    InstrumentKey: for<'de> Deserialize<'de>,
+    Kind: for<'de> Deserialize<'de>,
+{
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .map(|line_result| {
+            let line = line_result.unwrap();
+            serde_json::from_str::<MarketStreamEvent<InstrumentKey, Kind>>(&line).unwrap()
+        })
+        .collect()
+}
